3dfa6a7fce1343590f4b11210b78b92da6c5dae9
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-06-15 22:24:10 +0200

    add page layout and template

--- lib/sinetris_blog/controllers/pages.ex
+++ lib/sinetris_blog/controllers/pages.ex
@@ -1,7 +1,13 @@
 defmodule SinetrisBlog.Controllers.Pages do
   use Phoenix.Controller
+  use Sinetris.Renderer
 
   def index(conn) do
-    text conn, "Hello world"
+    text conn, "Hello world!!"
   end
+
+  def show(conn) do
+    render conn, "pages/show", [title: "Sinetris", page: conn.params["page"]]
+  end
+
 end
