ba0c175f35d1e56491d6e87f60f1be5731291315
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-06-16 14:27:51 +0200

    fix routing precedence

--- lib/sinetris_blog/router.ex
+++ lib/sinetris_blog/router.ex
@@ -2,6 +2,6 @@ defmodule SinetrisBlog.Router do
   use Phoenix.Router
 
   plug Plug.Static, at: "/static", from: :sinetris_blog
-  get "/", SinetrisBlog.Controllers.Pages, :index, as: :root
   get "/:page", SinetrisBlog.Controllers.Pages, :show, as: :page
+  get "/", SinetrisBlog.Controllers.Pages, :index, as: :root
 end
