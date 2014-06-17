2ceb0cb3945fd05bb8ce209279bbe5deee7e8f8e
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-06-17 02:51:12 +0200

    simplify renderer

--- lib/sinetris_blog/controllers/pages.ex
+++ lib/sinetris_blog/controllers/pages.ex
@@ -1,13 +1,13 @@
 defmodule SinetrisBlog.Controllers.Pages do
   use Phoenix.Controller
-  use Sinetris.Renderer
+  alias Sinetris.Renderer
 
   def index(conn) do
     text conn, "Hello world!!"
   end
 
   def show(conn) do
-    render conn, "pages/show", [title: "Sinetris", page: conn.params["page"]]
+    Renderer.render conn, "pages/show", [title: "Sinetris", page: conn.params["page"]]
   end
 
 end
