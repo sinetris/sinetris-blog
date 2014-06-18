16f472c3bbb3575a0b966cb6ec6226c4558078c8
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-06-18 19:39:21 +0200

    update elixir to v0.14.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SinetrisBlog.Mixfile do
   def project do
     [ app: :sinetris_blog,
       version: "0.0.1",
-      elixir: "~> 0.14.0",
+      elixir: "~> 0.14.1",
       deps: deps ]
   end
 
