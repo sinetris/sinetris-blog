029a827af6a2cbee976fbbd7d5f5967ac6cdcd47
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-07-02 14:05:17 +0200

    fix user tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,28 +3,28 @@ defmodule SinetrisBlog.UserTest do
   alias SinetrisBlog.User
 
   test "create user and auth" do
-    assert { :ok, %User{} } = User.create("eric", "eric1@.com", "hunter42")
+    assert { :ok, %User{} } = User.create("eric", "eric1@example.com", "hunter42")
     assert User.get("eric") |> User.auth?("hunter42")
   end
 
   test "create user and fail auth" do
-    assert { :ok, %User{} } = User.create("eric", "eric@mail.com", "erics_pass")
+    assert { :ok, %User{} } = User.create("eric", "eric@example.com", "erics_pass")
     refute User.get("josé") |> User.auth?("erics_pass")
     refute User.get("eric") |> User.auth?("wrong_pass")
   end
-  
+
   # test "users name and email are unique" do
   #   assert { :ok, %User{} } = User.create("eric", "eric@mail.com", "erics_pass")
   #   assert { :error, _ } = User.create("eric", "mail@mail.com", "pass")
   #   assert { :error, _ } = User.create("name", "eric@mail.com", "pass")
   # end
-  
+
   test "update user" do
-    assert { :ok, user } = User.create("eric", "eric@mail.com", "erics_pass")
-    User.update(user, "other@mail.com", "new_pass")
-  
+    assert { :ok, user } = User.create("eric", "eric@example.com", "erics_pass")
+    User.update(user, "other@example.com", "new_pass")
+
     user = User.get("eric")
-    assert user.email == "other@mail.com"
+    assert user.email == "other@example.com"
     assert User.auth?(user, "new_pass")
     refute User.auth?(user, "erics_pass")
   end
