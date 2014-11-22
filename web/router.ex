f124f4d80e7bd8bab8d83ce628eb833f6ce1cb5e
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2014-11-22 21:14:14 +0100

    use new router specs

--- web/router.ex
+++ web/router.ex
@@ -1,16 +1,31 @@
 defmodule SinetrisBlog.Router do
   use Phoenix.Router
 
-  plug Plug.Static, at: "/static", from: :sinetris_blog
-
-  plug Plug.MethodOverride
-
-  get "/:slug", SinetrisBlog.PagesController, :show, as: :page
-  get "/login", SinetrisBlog.SessionController, :new, as: :login
-  post "/login", SinetrisBlog.SessionController, :create, as: :create_login
-  delete "/logout", SinetrisBlog.SessionController, :destroy
-  get "/", SinetrisBlog.PagesController, :index, as: :root
-  scope path: "admin", alias: SinetrisBlog.Admin, helper: "admin" do
-    resources "pages", PagesController
+  pipeline :browser do
+    plug :accepts, ~w(html)
+    plug :fetch_session
   end
+
+  pipeline :api do
+    plug :accepts, ~w(json)
+  end
+
+  scope "/" do
+    # Use the default browser stack.
+    pipe_through :browser
+
+    get "/", SinetrisBlog.PagesController, :index, as: :pages
+    get "/login", SinetrisBlog.SessionController, :new, as: :login
+    post "/login", SinetrisBlog.SessionController, :create, as: :login
+    delete "/logout", SinetrisBlog.SessionController, :destroy
+    get "/:slug", SinetrisBlog.PagesController, :show, as: :page
+    scope path: "admin", alias: SinetrisBlog.Admin, helper: "admin" do
+        resources "pages", PagesController, as: :admin_page
+    end
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api" do
+  #   pipe_through :api
+  # end
 end
